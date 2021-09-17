7f2af3c0f2aaa65c8a626a6a697f02a2464f369c
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-18 08:08:57 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -1,16 +1,22 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response(JSON.stringify({
-      "import.meta.main": import.meta.main,
-      "import.meta.url": import.meta.url,
-      "Deno.cwd()": Deno.cwd(),
-      "event.request.url": event.request.url,
-    }), {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    }),
-  );
-});
+async function handleHttp(conn: Deno.Conn) {
+  for await (const e of Deno.serveHttp(conn)) {
+    e.respondWith(
+      new Response(JSON.stringify({
+        "import.meta.main": import.meta.main,
+        "import.meta.url": import.meta.url,
+        "Deno.cwd()": Deno.cwd(),
+        "event.request.url": event.request.url,
+      }), {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }),
+    )
+  }
+}
+
+for await (const conn of Deno.listen({ port: 80 })) {
+  handleHttp(conn);
+}
