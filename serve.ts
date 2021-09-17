2caf02c7f7ccbb3b04cd04a8a948e18ad9403546
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-18 07:49:35 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -1,6 +1,11 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
-    new Response(JSON.stringify({importMeta: import.meta.main, cwd: Deno.cwd(), url: event.request.url}), {
+    new Response(JSON.stringify({
+      importMetaMain: import.meta.main,
+      importMetaURL: import.meta.url,
+      cwd: Deno.cwd(),
+      url: event.request.url,
+    }), {
       status: 200,
       headers: {
         "content-type": "application/json",
