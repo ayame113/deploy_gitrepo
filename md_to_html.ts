27ba1194b08a1aeb38c9b7742989ae0c4f3016cd
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-19 05:22:58 +0900

    css

--- md_to_html.ts
+++ md_to_html.ts
@@ -4,7 +4,10 @@ await ammonia.init();
 
 export function mdToHTML({ content }: { content: string }) {
   return {
-    content: ammonia.clean(marked(content)),
+    content:
+      `<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"></head><body>${
+        ammonia.clean(marked(content))
+      }</body>`,
     headers: {
       "content-type": "text/html",
     },
