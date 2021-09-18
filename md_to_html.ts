b86352de70fd90a2dc36365c05729cde101cd704
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-19 06:26:40 +0900

    test

--- md_to_html.ts
+++ md_to_html.ts
@@ -1,42 +1,51 @@
 import marked from "https://esm.sh/marked";
-import * as ammonia from "https://deno.land/x/ammonia@0.3.1/mod.ts";
-//import "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js";
+import {
+  Ammonia,
+  AmmoniaBuilder,
+  init,
+} from "https://deno.land/x/ammonia@0.3.1/mod.ts";
 import hljs from "https://jspm.dev/highlight.js";
 
 marked.setOptions({
+  renderer: new marked.Renderer(),
+  langPrefix: "hljs language-",
   highlight: function (code, lang) {
-    console.log(code);
-    console.log(lang);
     // @ts-ignore
     const language = hljs.getLanguage(lang) ? lang : "plaintext";
     // @ts-ignore
     return hljs.highlight(code, { language }).value;
   },
+  gfm: true,
 });
-await ammonia.init();
+await init();
+const builder = new AmmoniaBuilder();
+builder.genericAttributes.add("class");
+builder.build();
+const ammonia = new Ammonia(builder);
 
 export function mdToHTML({ content }: { content: string }) {
   return {
     content: `<head>
-			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css">
-			<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.2.0/styles/default.min.css">
-			</head><body class="markdown-body">${
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css">
+      <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.2.0/styles/default.min.css">
+      </head><body class="markdown-body">${
       ammonia.clean(marked(content))
     }</body>`,
     headers: {
       "content-type": "text/html",
     },
   };
-}
+} /*
 console.log(
   mdToHTML({
     content: `
 # a
 
-\`\`\`ts
+\`\`\`js
 
 console.log('aaa');
 \`\`\`
 	`,
   }).content,
 );
+*/
